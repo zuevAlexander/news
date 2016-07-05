6e114c11c989ce84faeb9334421dfda9fd0b2ba3
Zuev Alexander <zuev22@gmail.com>
2016-07-05 23:09:35 +0300

    add menu and post models

---
+++ test2.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Александр
+ * Date: 26.06.2016
+ * Time: 22:42
+ */
+include_once("model/post.php");
+
+$a = new Post();
+$b = 2;
+$c= $a->getAll();
+var_dump($c);
